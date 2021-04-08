70651931cb82e201095a6006e0cdc2869b0d51ca
Beasley <beasley.com@gmail.com>
2021-04-08 00:33:48 -0700

    idea for another script

---
+++ testco.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#The idea here is to be able to export a variable name that get's updated everytime cochalle(challengeName) is called with the name of the code challenge.
+# actually I think it makes more sense to break down the return from pwd than rely on the last time cochalle was called ---> maybe I want to use this script when I'm navigating around!  
