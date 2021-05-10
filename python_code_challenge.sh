61b1dfd9af669827e9e818ba27ed350e703d10a8
Beasley <beasley.com@gmail.com>
2021-05-10 15:09:28 -0700

    demonstrated some script action with seth

--- python_code_challenge.sh
+++ python_code_challenge.sh
@@ -3,7 +3,10 @@ echo "What's the name of this code challenge? (snake_case)"
 read name
 mkdir $name
 cd $name
+mkdir tests
+cd tests
 code .
 touch $name.py
-touch $name.test.py
+touch test_$name.py
 echo "YOLO, Have Fun!"
+
